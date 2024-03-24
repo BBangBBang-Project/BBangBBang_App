27464e1e0f97bfde357144cb2acd20e76335ce88
soohee Jang <shjang0820@hansung.ac.kr>
2024-03-25 01:10:59 +0900

    장바구니 화면 속 데이터 연결 완료 - 상품 수정 부분 아직 미완

--- src/screens/Cart/CartScreen.js
+++ src/screens/Cart/CartScreen.js
@@ -1,82 +1,147 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import DropDownPicker from 'react-native-dropdown-picker';
 import EmptyCart from './components/EmptyCart';
 import CartList from './components/CartList';
+import axios from 'axios';
 const CartScreen = ({navigation}) => {
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState(null);
-    const [items, setItems] = useState([
-        {label: '한성대 입구역', value: '한성'},
-        {label: '혜화역', value: '혜화'},
-        {label: '성신여대역', value: '성신'},
-    ]);
-    const [cartItems, setCartItems] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: '한성대 입구역', value: '한성'},
+    {label: '혜화역', value: '혜화'},
+    {label: '성신여대역', value: '성신'},
+  ]);
+  const [cartItems, setCartItems] = useState([]);
 
-    return (
-        <View style={styles.cartScreenContainer}> 
-            <View style={[styles.titleContainer, { borderBottomColor: '#949393', borderBottomWidth: 1}]}>
-                <TouchableOpacity onPress={() => navigation.goBack()}>
-                    <Icon style={styles.goBackButton} name="chevron-back-outline"></Icon>
-                </TouchableOpacity>
-                <Text style={styles.titleText}>장바구니</Text>
-                </View>
-                <DropDownPicker
-                    open={open}
-                    value={value}
-                    items={items}
-                    setOpen={setOpen}
-                    setValue={setValue}
-                    setItems={setItems}
-                    style={styles.dropDown}
-                    dropDownContainerStyle={{ borderWidth: 1, borderTopWidth :1, borderColor: 'black', width : 250, alignSelf: 'center', marginTop : 20}}
-                    textStyle={{ fontSize: 20 }}
-                    listItemContainerStyle={{ height: 40 }}
-                    zIndex={3000}
-                    zIndexInverse={1000}
-                    placeholder='지점을 선택해주세요'
-                />
-                <ScrollView contentContainerStyle={{ paddingBottom: 100 }}>
-               <CartList/>
-               <CartList/>
-               <CartList/>
-               <CartList/>
-               </ScrollView>
-        </View>
-    );
+  useEffect(() => {
+    // 장바구니 데이터를 가져오는 함수
+    const fetchCartItems = async () => {
+      try {
+        const customerId = '1'; // 예시로 '1'을 사용
+        const response = await axios.get(
+          `http://localhost:8080/customer/${customerId}/cart`,
+        );
+        setCartItems(response.data); // 응답 데이터를 상태에 저장
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchCartItems(); // 함수 호출
+  }, []);
+  // 항목의 수량을 서버에 업데이트하는 함수
+  const updateItemQuantity = async (customerId, cartItemId, newQuantity) => {
+    try {
+      const response = await axios.patch(
+        `http://localhost:8080/customer/${customerId}/cart/items/${cartItemId}/${newQuantity}`,
+        {quantity: newQuantity}, // 요청 바디에 quantity를 포함시킴
+      );
+      console.log('수량 업데이트 성공:', response.data);
+    } catch (error) {
+      console.error('수량 업데이트 실패:', error);
+    }
+  };
+  // 항목의 수량을 업데이트하는 함수
+  const handleQuantityChange = (itemId, newQuantity) => {
+    const customerId = '1';
+    updateItemQuantity(customerId, itemId, newQuantity); // 서버에 수량 업데이트 요청
+    const updatedCartItems = cartItems.map(item => {
+      if (item.id === itemId) {
+        return {...item, quantity: newQuantity};
+      }
+      return item;
+    });
+    setCartItems(updatedCartItems);
+  };
+
+  return (
+    <View style={styles.cartScreenContainer}>
+      <View
+        style={[
+          styles.titleContainer,
+          {borderBottomColor: '#949393', borderBottomWidth: 1},
+        ]}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Icon style={styles.goBackButton} name="chevron-back-outline"></Icon>
+        </TouchableOpacity>
+        <Text style={styles.titleText}>장바구니</Text>
+      </View>
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        style={styles.dropDown}
+        dropDownContainerStyle={{
+          borderWidth: 1,
+          borderTopWidth: 1,
+          borderColor: 'black',
+          width: 250,
+          alignSelf: 'center',
+          marginTop: 20,
+        }}
+        textStyle={{fontSize: 20}}
+        listItemContainerStyle={{height: 40}}
+        zIndex={3000}
+        zIndexInverse={1000}
+        placeholder="지점을 선택해주세요"
+      />
+      <ScrollView contentContainerStyle={{paddingBottom: 100}}>
+        {cartItems.length > 0 ? (
+          cartItems.map((item, index) => (
+            <CartList
+              key={index}
+              item={item}
+              onQuantityChange={handleQuantityChange}
+            />
+          ))
+        ) : (
+          <EmptyCart />
+        )}
+      </ScrollView>
+    </View>
+  );
 };
-// {cartItems.length === 0 ? <EmptyCart/> : <CartList cartItems={cartItems} />}
 const styles = StyleSheet.create({
-    cartScreenContainer: {
-        flex: 1,
-        backgroundColor: 'white',
-        justifyContent: 'flex-start',
-    },
-    titleContainer: {
-        justifyContent: 'flex-start',
-        alignContent: 'center',
-        flexDirection: 'row',
-        marginTop: 50,
-    },
-    goBackButton: {
-        marginLeft: 10,
-        fontSize: 40,
-        marginBottom: 20,
-    },
-    titleText: {
-        marginLeft: 20,
-        fontSize: 30,
-        fontFamily: 'Syncopate',
-    },
-    dropDown : {
-        marginTop : 20,
-        width : 250,
-        alignSelf: 'center',
-        borderWidth : 0,
-        borderBottomWidth: 1,
-        borderColor: 'black',
-    },
+  cartScreenContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'flex-start',
+  },
+  titleContainer: {
+    justifyContent: 'flex-start',
+    alignContent: 'center',
+    flexDirection: 'row',
+    marginTop: 50,
+  },
+  goBackButton: {
+    marginLeft: 10,
+    fontSize: 40,
+    marginBottom: 20,
+  },
+  titleText: {
+    marginLeft: 20,
+    fontSize: 30,
+    fontFamily: 'Syncopate',
+  },
+  dropDown: {
+    marginTop: 20,
+    width: 250,
+    alignSelf: 'center',
+    borderWidth: 0,
+    borderBottomWidth: 1,
+    borderColor: 'black',
+  },
 });
 
 export default CartScreen;
--- src/screens/Cart/components/CartList.js
+++ src/screens/Cart/components/CartList.js
@@ -1,119 +1,129 @@
-import React, { useState } from 'react';
-import { View, TouchableOpacity, StyleSheet,Text,Image } from 'react-native';
+import React, {useState} from 'react';
+import {View, TouchableOpacity, StyleSheet, Text, Image} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Icon2 from 'react-native-vector-icons/EvilIcons';
 
-const CartList = () => {
+const CartList = ({item, onQuantityChange}) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const toggleCheckbox = () => {
+    setIsChecked(!isChecked);
+  };
 
-    const [isChecked, setIsChecked] = useState(false);
-    const toggleCheckbox = () => {
-        setIsChecked(!isChecked);
-    };
+  const [quantity, setQuantity] = useState(item.quantity);
 
-    const [quantity, setQuantity] = useState(1);
-    const handleQuantityChange = (type) => {
-      if(type === 'increase') {
-        setQuantity(prevQuantity => prevQuantity + 1);
-      } else if(type === 'decrease' && quantity > 1) {
-        setQuantity(prevQuantity => prevQuantity - 1);
-      }
-    };
+  const handleQuantityChange = type => {
+    let newQuantity = quantity;
+    if (type === 'increase') {
+      newQuantity = quantity + 1;
+    } else if (type === 'decrease' && quantity > 1) {
+      newQuantity = quantity - 1;
+    }
 
-    return (
-        <View style = {[styles.cartListContainer, { borderBottomColor: '#949393', borderBottomWidth: 1}]}>
+    setQuantity(newQuantity);
+    onQuantityChange(item.id, newQuantity);
+  };
+  return (
+    <View
+      style={[
+        styles.cartListContainer,
+        {borderBottomColor: '#949393', borderBottomWidth: 1},
+      ]}>
+      <TouchableOpacity onPress={toggleCheckbox}>
+        <Icon
+          style={styles.checkbox}
+          name={isChecked ? 'checkbox' : 'checkbox-outline'}></Icon>
+      </TouchableOpacity>
 
-            <TouchableOpacity onPress={toggleCheckbox}>
-                <Icon style={styles.checkbox} name={isChecked ? "checkbox-outline" : "checkbox"}></Icon>
-            </TouchableOpacity>
-            
-            <View style = {styles.rowCartContainer}>
-            <View style = {styles.listImageContainer}>
-                <Image style = {styles.listImage}source={require('../../../assets/images/bread.png')}/>
-            </View>
-            <Text style = {styles.listName}>쫄깃쫄깃 식빵</Text>
-            <View style={styles.quantityBox}>
-                    <TouchableOpacity onPress={() => handleQuantityChange('decrease')}>
-                        <Icon2 name = 'minus'style={styles.controlButton}></Icon2>
-                    </TouchableOpacity>
-                    <Text style={styles.quantity}>{quantity}</Text>
-                    <TouchableOpacity onPress={() => handleQuantityChange('increase')}>
-                    <Icon2 name = 'plus'style={styles.controlButton}></Icon2>
-                    </TouchableOpacity>          
+      <View style={styles.rowCartContainer}>
+        <View style={styles.listImageContainer}>
+          <Image
+            style={styles.listImage}
+            source={require('../../../assets/images/bread.png')}
+          />
         </View>
-            <View style = {styles.priceContainer}>
-            <Text style={styles.salePrice}>2,500원</Text>
-            <Text style={styles.originalPrice}>5,000원</Text>  
-            </View>
-            </View>
+        <Text style={styles.listName}>{item.productName}</Text>
+        <View style={styles.quantityBox}>
+          <TouchableOpacity onPress={() => handleQuantityChange('decrease')}>
+            <Icon2 name="minus" style={styles.controlButton}></Icon2>
+          </TouchableOpacity>
+          <Text style={styles.quantity}>{quantity}</Text>
+          <TouchableOpacity onPress={() => handleQuantityChange('increase')}>
+            <Icon2 name="plus" style={styles.controlButton}></Icon2>
+          </TouchableOpacity>
         </View>
-    );
+        <View style={styles.priceContainer}>
+          <Text style={styles.salePrice}>{item.price}</Text>
+          <Text style={styles.originalPrice}>{item.price}</Text>
+        </View>
+      </View>
+    </View>
+  );
 };
 
-
-
 const styles = StyleSheet.create({
-    cartListContainer: {
-        flex : 1,
-        backgroundColor : 'white',
-    },
-    rowCartContainer : {
-        flexDirection: 'row',
-    },
-    checkbox : {
-        marginTop : 10,
-        marginLeft : 15,
-        fontSize : 23,
-        color : '#FF7E7E',
-    },
-    listImageContainer : {
-        marginTop : 10,
-        marginLeft : 30,
-        marginBottom : 10,
-        width : 90,
-        height : 90,
-        borderRadius: 45,
-        backgroundColor : '#FAEBE1',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    listImage : {
-        width: '70%',
-        height: '70%',
-        resizeMode : 'contain',
-    },
-    listName : {
-        fontSize : 20,
-        marginLeft : 50,
-    },
-    salePrice : {
-        marginTop : 35,
-        marginLeft : 30,
-        color: 'rgba(225, 36, 36, 0.66)',
-        fontSize : 17,       
-    },  
-    originalPrice :{
-        marginTop : 0,
-        textDecorationLine: 'line-through',
-        marginLeft : 35,
-        fontSize : 12,
-        color : '#9A9A9A',
-    },
-      quantityBox: {
-        marginLeft : -130,
-        marginTop : 30,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingVertical : 10,
-      },
-      
-      controlButton: {
-        fontSize: 23,
-        marginHorizontal: 10,
-      },
-      quantity: {
-        fontSize: 17,
-      },
+  cartListContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  rowCartContainer: {
+    flexDirection: 'row',
+  },
+  checkbox: {
+    marginTop: 10,
+    marginLeft: 15,
+    fontSize: 23,
+    color: '#FF7E7E',
+  },
+  listImageContainer: {
+    marginTop: 10,
+    marginLeft: 50,
+    marginBottom: 10,
+    marginRight: 40,
+    width: 90,
+    height: 90,
+    borderRadius: 45,
+    backgroundColor: '#FAEBE1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listImage: {
+    width: '70%',
+    height: '70%',
+    resizeMode: 'contain',
+  },
+  listName: {
+    fontSize: 20,
+    marginLeft: 30,
+  },
+  salePrice: {
+    marginTop: 35,
+    marginLeft: 30,
+    color: 'rgba(225, 36, 36, 0.66)',
+    fontSize: 17,
+  },
+  originalPrice: {
+    marginTop: 0,
+    textDecorationLine: 'line-through',
+    marginLeft: 35,
+    fontSize: 12,
+    color: '#9A9A9A',
+  },
+  quantityBox: {
+    marginLeft: -80,
+    marginTop: 30,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+
+  controlButton: {
+    fontSize: 23,
+    marginHorizontal: 10,
+  },
+  quantity: {
+    fontSize: 17,
+  },
 });
 
 export default CartList;
