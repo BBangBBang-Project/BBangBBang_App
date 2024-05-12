510db952230de30f861891a45290c0732b10263d
soohee Jang <shjang0820@hansung.ac.kr>
2024-05-13 01:42:15 +0900

    add Google Map api
    api key 다 숨김

--- ios/bbangbbangApp/AppDelegate.mm
+++ ios/bbangbbangApp/AppDelegate.mm
@@ -2,11 +2,14 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import "RNSplashScreen.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  NSString *apiKey = [self googleMapsApiKey];
+      [GMSServices provideAPIKey:apiKey];
   self.moduleName = @"bbangbbangApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
@@ -15,6 +18,16 @@
 [super application:application didFinishLaunchingWithOptions:launchOptions];
   [RNSplashScreen show];
   return YES;
+}
+- (NSString *)googleMapsApiKey {
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"MapInfo" ofType:@"plist"];
+    NSDictionary *config = [NSDictionary dictionaryWithContentsOfFile:path];
+    NSString *apiKey = [config objectForKey:@"GoogleMapsAPIKey"];
+    if (!apiKey) {
+        NSLog(@"API key is not set or Config.plist is missing");
+        return @"";
+    }
+    return apiKey;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
