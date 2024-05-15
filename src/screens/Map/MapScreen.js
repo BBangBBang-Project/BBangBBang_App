8559ef147263dd9183f427a7ea94ad2e3354fde5
soohee Jang <shjang0820@hansung.ac.kr>
2024-05-15 19:46:19 +0900

    make mapScreen with marker

--- src/screens/Map/MapScreen.js
+++ src/screens/Map/MapScreen.js
@@ -1,24 +1,141 @@
-import React from "react";
-import { View, Text } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Platform, PermissionsAndroid, TouchableOpacity } from "react-native";
+import Geolocation from "react-native-geolocation-service";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from '@react-navigation/native';
+
+async function requestPermission() {
+  try {
+    if (Platform.OS === "ios") {
+      const status = await Geolocation.requestAuthorization("always");
+      return status === "granted";
+    } else if (Platform.OS === "android") {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+      return granted === PermissionsAndroid.RESULTS.GRANTED;
+    }
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+}
 
 function MapScreen() {
-  return (
-    <>
-      <View style={{ flex: 1 }}>
-        <MapView
-          style={{ flex: 1 }}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        />
+  const [location, setLocation] = useState(null);
+  const navigation = useNavigation();
+
+  const getLocation = async () => {
+    const hasPermission = await requestPermission();
+    if (hasPermission) {
+      Geolocation.getCurrentPosition(
+        (position) => {
+          console.log("Current position: ", position);
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.007,
+            longitudeDelta: 0.007,
+          });
+        },
+        (error) => {
+          console.log("Error getting current position: ", error);
+        },
+        {
+          enableHighAccuracy: true,
+          timeout: 15000,
+          maximumAge: 10000,
+        },
+      );
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  if (!location) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Splash Screen</Text>
       </View>
-    </>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.goBackButton} onPress={() => navigation.goBack()}>
+        <Icon name="chevron-back-outline" style={styles.icon} />
+      </TouchableOpacity>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={location}
+        showsUserLocation={true}
+      >
+        <Marker
+          coordinate={{ latitude: 37.58840929637797, longitude: 127.00602347678497 }}
+          title={"한성대입구역"}
+          description={"한성대입구역"}
+          icon={require('../../assets/images/marker.png')}
+        />
+        <Marker
+          coordinate={{ latitude: 37.592694767073134, longitude: 127.01650323455772 }}
+          title={"성신여대입구역"}
+          description={"성신여대입구역"}
+          icon={require('../../assets/images/marker.png')}
+        />
+        <Marker
+          coordinate={{ latitude: 37.58215429037538, longitude: 127.00189633072232 }}
+          title={"혜화역"}
+          description={"혜화역"}
+          icon={require('../../assets/images/marker.png')}
+        />
+      </MapView>
+      <TouchableOpacity style={styles.button} onPress={getLocation}>
+        <Text style={styles.buttonText}>현재 위치 가져오기</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    textAlign: "center",
+    marginTop: 20,
+  },
+  map: {
+    flex: 1,
+  },
+  goBackButton: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    zIndex: 1,
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    borderRadius: 25,
+    padding: 5,
+  },
+  icon: {
+    fontSize: 30,
+  },
+  button: {
+    position: 'absolute',
+    bottom: 20,
+    left: '50%',
+    transform: [{ translateX: -75 }],
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
 
 export default MapScreen;
