5fa28cc1d004d21267110fcd9510dfa85d88dcb3
soohee Jang <shjang0820@hansung.ac.kr>
2024-05-27 15:53:29 +0900

    update Header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, Image, TouchableOpacity, Dimensions} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/EvilIcons';
 import Icon2 from 'react-native-vector-icons/Feather';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+const { width } = Dimensions.get('window'); // 화면의 가로 길이를 가져옴
+
 const Header = () => {
     const navigation = useNavigation();
     
@@ -17,16 +21,18 @@ const Header = () => {
         />
         <Text style={styles.logotext}>BBANGBBANG</Text>
       </TouchableOpacity>
+      <View style={styles.iconContainer}>
       <TouchableOpacity onPress={() => navigation.navigate('Map')}>
-      <Icon style={styles.search} name="search" size={30} color="black"></Icon>
+      <Icon style={styles.search} name="search" size={width * 0.08} color="black"></Icon>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => navigation.navigate('Cart')}>
       <Icon2
         style={styles.icon}
         name="shopping-bag"
-        size={23}
+        size={width * 0.06}
         color="black"></Icon2>
         </TouchableOpacity>
+    </View>
     </View>
     </SafeAreaView>
   );
@@ -35,34 +41,38 @@ const Header = () => {
 const styles = StyleSheet.create({
   safeArea: {
     backgroundColor: '#ffffff',
+    marginTop: width * 0.03
   },
   container: {
     backgroundColor: '#ffffff',
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: width * 0.05,
+    height: 60,
   },
   logoContainer: {
     flexDirection: 'row',
     alignItems: 'center',
   },
   image: {
-    marginTop: 30,
-    marginLeft: 24,
-    height: 35,
-    width: 35,
+    height: width * 0.08,
+    width: width * 0.08,
   },
   logotext: {
-    marginTop: 40,
     marginLeft: 10,
-    fontSize: 15,
+    fontSize: width * 0.042,
     fontFamily: 'Syncopate',
   },
+  iconContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   search: {
-    marginTop: 34,
-    marginLeft: 105,
+    marginRight: width * 0.03,
   },
   icon: {
-    marginTop: 34,
-    marginLeft: 8,
+    marginRight: 0,
   },
 });
 export default Header;
